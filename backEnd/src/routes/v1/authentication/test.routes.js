6015219427a468cb892a11baf628e3a4fc9d9833
Santiago <Santiagogbf2000@Gmail.com>
2024-01-26 10:27:40 -0400

    Making organitation

---
+++ backEnd/src/routes/v1/authentication/test.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { test } from '../../../controller/v1/test.controller.js'
+import { userExtractor } from '../../../middleware/userExtractor.js'
+import { adminVerification } from '../../../middleware/rolVerification.js'
+
+const router = Router()
+
+router.get('/', userExtractor, adminVerification, test)
+
+export default router
