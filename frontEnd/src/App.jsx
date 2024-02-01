19d0105bf6bc6b9a444c2b80dc89550250714898
Santiago <Santiagogbf2000@Gmail.com>
2024-02-01 12:54:32 -0400

    Added: logo

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,13 +5,15 @@ import { representante } from './constants/dataStructure'
 import { representanteValidation } from './constants/dataValidation'
 import { validationInputRepresentante } from './constants/validationInputs'
 import { representanteFields } from './constants/form/fields'
+import HeaderSidebar from './components/partials/header-side'
+import { ConfigProvider } from 'antd'
 
 function App () {
   const { formData, actions, errorState } = useMyFormHook(representante, representanteValidation, validationInputRepresentante)
 
   return (
     <>
-      <MyForm title='Example' fields={representanteFields} formData={formData} actions={actions} errorMessage={errorState} />
+      <HeaderSidebar />
     </>
   )
 }
