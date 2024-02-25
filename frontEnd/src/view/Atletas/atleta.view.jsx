3308217b684e7f8d8596823e516e7f2f5f6c98b2
Andrés Pausín <111588426+andrespausin@users.noreply.github.com>
2024-02-25 19:24:50 -0400

    fix: colors design

--- frontEnd/src/view/Atletas/atleta.view.jsx
+++ frontEnd/src/view/Atletas/atleta.view.jsx
@@ -9,15 +9,15 @@ const AtletaView = () => {
   console.log(atleta)
   return (
     <>
-      <Stack direction='row' w='100%' h='100%' bg='black'>
-        <Stack bg='yellow' p={10} align='center' w='50%' h='100%'>
+      <Stack direction='row' w='100%' h='100%' gap='1%'>
+        <Stack bg='white' rounded='15px' p={10} align='center' w='35%' h='100%'>
           <MyAtletaDatos data={atleta.datosGeneral} />
         </Stack>
-        <VStack bg='green' w='50%' h='100%'>
-          <Stack bg='red' w='100%' h='100%'>
+        <VStack rounded='10px' w='100%' h='100%' gap={4}>
+          <Stack bg='red' w='100%' h='100%' rounded='10px'>
             <MyAtletaEstadisticas data={atleta.estadisticas} />
           </Stack>
-          <Stack bg='blue' w='100%' h='100%'>
+          <Stack bg='blue' w='100%' h='100%' rounded='10px'>
             Algunas medidas antropometricas
           </Stack>
         </VStack>
