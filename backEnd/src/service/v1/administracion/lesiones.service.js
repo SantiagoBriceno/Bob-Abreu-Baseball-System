0712e21eb02dab80f2ff414c0d34c88b17e3eafe
Andres Pausin <candrespausin2001@gmail.com>
2024-03-10 13:00:39 -0400

    create: lesiones service

---
+++ backEnd/src/service/v1/administracion/lesiones.service.js
@@ -0,0 +1,37 @@
+import { pool } from '../../../db.js'
+
+/* Lesiones */
+
+const getAllLesiones = async () => {
+  const [lesiones] = await pool.query('SELECT * FROM lesiones')
+  return lesiones
+}
+
+const getLesionByIdPlayer = async (id) => {
+  const [lesion] = await pool.query('SELECT * FROM lesiones WHERE id_atleta = ?', [id])
+  return lesion
+}
+
+const createLesion = async (lesion) => {
+  const [newLesion] = await pool.query('INSERT INTO lesiones SET ?', [lesion])
+  return newLesion
+}
+
+const updateLesion = async (id, lesion) => {
+  const [updatedLesion] = await pool.query('UPDATE lesiones SET ? WHERE id = ?', [lesion, id])
+  return updatedLesion
+}
+
+const deleteLesion = async (id) => {
+  const [lesion] = await pool.query('SELECT * FROM lesiones WHERE id = ?', [id])
+  await pool.query('DELETE FROM lesiones WHERE id = ?', [id])
+  return lesion
+}
+
+export default {
+  getAllLesiones,
+  getLesionByIdPlayer,
+  createLesion,
+  updateLesion,
+  deleteLesion
+}
